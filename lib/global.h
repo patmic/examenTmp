3bfd4314cd0e50063a26a69e298297b3627ad7ee
pat_mic <pat_mic@hotmail.com>
2022-07-01 12:44:28 -0500

    archivos inciiales

---
+++ lib/global.h
@@ -0,0 +1,6 @@
+#define TAB    '\t'     
+#define ENTER  '\n'
+#define PI  3.1416
+#define G  9.8
+
+enum MES { ENE = 1, FEB, MAR, ABR, MAY, JUN, JUL, AGO, SEP, OCT, NOV, DIC };
---
+++ src/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>		// entradas salidas en consola y string
+#include <fstream> 		// manejo de archivos
+#include "../lib/global.h"
+
+using namespace std;
+
+void leerArchivo(string pathFile)
+{
+	string s;
+    fstream f;
+
+	cout << "leer archivo"  << endl;
+    f.open(pathFile, ios_base::in);  // in  out app 
+    if ( !f.is_open() ) 
+		cerr << "Error al abrir el archivo: " << pathFile << endl;
+	else
+		do 
+		{
+			getline( f, s );
+			cout << s << endl;
+		}while( !f.eof() );
+	f.close();
+}
+
+void escribirArchivo(string pathFile)
+{
+    fstream f;
+	cout << "escribir archivo"  << endl;
+    f.open(pathFile, ios_base::app | ios_base::in );  // in  out app 
+    if ( !f.is_open() ) 
+		cerr << "Error al abrir el archivo: " << pathFile << endl;
+	else
+	{
+		for (int i = 1; i < 12; i++)
+		{
+			switch (i)
+			{
+				case ENE : f << "enero" 	<< ENTER; break;
+				case FEB : f << "febrero" << ENTER; break;
+				case MAR : f << "marzo" 	<< ENTER; break;
+				case ABR : f << "abril" 	<< ENTER; break;
+				case MAY : f << "mayo" 	<< ENTER; break;
+				default:
+					f << "otro mes" << ENTER;
+					break;
+			}
+		}
+	}
+	f.close();
+}
+
+int main()
+{
+	escribirArchivo("files/texto.txt");
+	leerArchivo("files/texto.txt");
+
+	// int a=10;
+	// printf("ingrese el valor de a = ");
+	// scanf("%d", &a);	
+	// printf("a= %c \n", a);
+
+	// cout << "ingrese el valor de a = ";
+	// cin >> a;
+	// cout << "a= " << a << endl;
+
+	// cout << "Hello pat-mic...!" << endl;
+	// return 0;
+}
